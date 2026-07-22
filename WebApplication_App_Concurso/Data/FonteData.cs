63454929739fd079b1872101a6c7765e5b97df7b
Sandro Eduardo Fracaro <sandrof450@gmail.com>
2026-07-22 14:52:14 -0300

    fix: Correção dos valores quando vierem dados com espaços e caracteres não necessários.

---
+++ WebApplication_App_Concurso/Data/FonteData.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace WebApplication_App_Concurso.Data
+{
+    public class FonteData
+    {
+        public static async Task<List<string>> GetAllFontesAsync() => new()
+        {
+            "PCI Concursos",
+            "Concursos no Brasil",
+        };
+    }
+}
